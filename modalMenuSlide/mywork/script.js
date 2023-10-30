2a36a715fdcaa9f341f9f846370cfe8348e3ff89
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-30 11:09:10 +0900

    feat: 토클버튼누르면은 nav바가 슬라이딩으로 생김

--- modalMenuSlide/mywork/script.js
+++ modalMenuSlide/mywork/script.js
@@ -1,6 +1,7 @@
 const signUpButton = document.querySelector(".modal__button");
 const closeButton = document.querySelector(".modal-close");
 const modalContainer = document.querySelector(".modal__container");
+const toggleButton = document.querySelector(".header__toggle__button");
 
 signUpButton.addEventListener("click", () => {
   modalContainer.classList.add("show-modal");
@@ -8,4 +9,13 @@ signUpButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   modalContainer.classList.remove("show-modal");
+});
+
+function onToggle() {
+  const body = document.querySelector("body");
+  body.toggleAttribute("show-nav");
+}
+
+toggleButton.addEventListener("click", (e) => {
+  onToggle();
 });
