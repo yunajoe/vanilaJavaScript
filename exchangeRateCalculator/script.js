e92cbc0e5e859746aa102e138e8c2125deb14d39
yunajoe <yunajoe@gmail.com>
2024-08-06 05:57:07 +0900

    feat: completed the exchangeRateCaculator

--- exchangeRateCalculator/script.js
+++ exchangeRateCalculator/script.js
@@ -1,62 +1,92 @@
-// function calculate() {
-//   //    fetch("items.json").then((res) => console.log(res) 일때 Response {type: 'basic', url: 'http://127.0.0.1:5500/items.json', redirected: false, status: 200, ok: true, …}
-//   //  fetch("items.json").then((res) => console.log(res.json()));때는 return값이 Promise [pending]이다
-//   //   fetch("items.json")
-//   //   .then((res) => res.json())
-//   //   .then((data) => console.log(data)); 때는 [{}, {}, {}]
-//   // }
-// }
+const before_select = document.querySelector("#exchange-before-select"); // USD
+const after_select = document.querySelector("#exchange-after-select"); // BGN
+const cal_before_input = document.querySelector("#before-input");
+const cal_after_input = document.querySelector("#after-input"); // <input id="after-input" type="number">
+const cal_texts = document.querySelector("#exchange-calculate-value");
+const swap_button = document.querySelector("#swap-button");
+let swap_before_value;
+swap_before_value = before_select.value; // USD
+let swap_after_value;
+swap_after_value = after_select.value; // BGN
+let fromCurrency = 0;
+let toCurrency = 0;
+let Total = 0;
+let url =
+  "https://v6.exchangerate-api.com/v6/db7b8e53e9914c5c9d0d3740/latest/USD";
 
-// calculate();
-
-// 환전하기전 select 태그
-const currencyEl_one = document.getElementById("currency-one");
-// 환전하기전 input태그
-const amountEl_one = document.getElementById("amount-one");
-// 환전한후 select태그
-const currencyEl_two = document.getElementById("currency-two");
-// 환전한후 input태그
-const amountEl_two = document.getElementById("amount-two");
-
-// swap버튼
-const swap = document.getElementById("swap");
-
-// 얘뭐지
-const rateEl = document.getElementById("rate");
-
-function calculate() {
-  const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
-  // console.log(currency_one, currency_two); // USD AED
-  // console.log(fecth())를 하면은 promise 객체가 return이 된다
-  fetch(
-    `https://v6.exchangerate-api.com/v6/db7b8e53e9914c5c9d0d3740/latest/${currency_one}`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      //  currency one을 currencytwo로 바꿀떄 value값
-      const rate = data.conversion_rates[currency_two];
-      //   1 currency_one단위 = 2 cureency_one단위
-      // 예를 들어 1 USD = 0.232323 EXR
-      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+async function fetchData() {
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const jsonData = await response.json();
+    return jsonData;
+  } else {
+    throw new Error("오류가 났네욤");
+  }
 }
 
-// 이벤트 리스너
-// currencyEl_one.addEventListener("change", calculate());
-// select메뉴가 변할때
-currencyEl_one.addEventListener("change", calculate);
-// 인풋값이 변할때
-amountEl_one.addEventListener("input", calculate);
-currencyEl_two.addEventListener("change", calculate);
-amountEl_two.addEventListener("input", calculate);
+let unitNumber = cal_before_input.value;
+displayData(unitNumber, swap_before_value, swap_after_value);
 
-// 환율변경하는 전, 후 select바꾸기
+async function displayData(unitNumber, swap_before_value, swap_after_value) {
+  let jsonObject = await fetchData();
+  fromCurrency = jsonObject.conversion_rates[swap_before_value];
+  toCurrency = jsonObject.conversion_rates[swap_after_value];
 
-swap.addEventListener("click", function () {
-  const temp = currencyEl_one.value;
-  currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
-  calculate();
+  swap_button.addEventListener("click", function (event) {
+    let temp = swap_before_value;
+    swap_before_value = swap_after_value;
+    swap_after_value = temp;
+
+    before_select.value = swap_before_value;
+    after_select.value = swap_after_value;
+    return displaySwapData(
+      jsonObject,
+      unitNumber,
+      swap_before_value,
+      swap_after_value
+    );
+  });
+  return displaySwapData(
+    jsonObject,
+    unitNumber,
+    swap_before_value,
+    swap_after_value
+  );
+  // Total = (fromCurrency * toCurrency).toFixed(3);
+  // cal_after_input.value = (Total * unitNumber).toFixed(3);
+  // cal_texts.textContent = `기본단위 1unit >>> 1 ${swap_before_value} =  ${Total} ${swap_after_value}`;
+}
+
+before_select.addEventListener("change", function (event) {
+  swap_before_value = before_select.value;
+  swap_after_value = after_select.value;
+  displayData(unitNumber, swap_before_value, swap_after_value);
 });
+
+after_select.addEventListener("change", function (event) {
+  swap_after_value = event.target.value;
+  swap_before_value = before_select.value;
+  swap_after_value = after_select.value;
+
+  displayData(unitNumber, swap_before_value, swap_after_value);
+});
+
+cal_before_input.addEventListener("change", function (event) {
+  unitNumber = +event.target.value;
+  swap_before_value = before_select.value;
+  swap_after_value = after_select.value;
+  displayData(unitNumber, swap_before_value, swap_after_value);
+});
+
+function displaySwapData(
+  jsonObject,
+  unitNumber,
+  swap_before_value,
+  swap_after_value
+) {
+  fromCurrency = jsonObject.conversion_rates[swap_before_value];
+  toCurrency = jsonObject.conversion_rates[swap_after_value];
+  const swapCurrency = (toCurrency / fromCurrency).toFixed(3);
+  cal_after_input.value = (swapCurrency * unitNumber).toFixed(3);
+  cal_texts.textContent = `1 ${swap_before_value} =  ${swapCurrency} ${swap_after_value}`;
+}
